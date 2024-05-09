0dcfbea387c3a66b3858b40833a315eba9c7a6ec
mounir-said <mou.sai.ms3@gmail.com>
2024-05-09 20:52:43 +0100

    Update React submodule

---
+++ React/First React With JSX/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Dojo!</h1>
+      <h2>I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
