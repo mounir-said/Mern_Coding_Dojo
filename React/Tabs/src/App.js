530010d4b9b728b6fa8c7601302931d710e7976b
mounir-said <mou.sai.ms3@gmail.com>
2024-06-02 21:40:55 +0100

    tabs

---
+++ React/Tabs/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Tabs from './components/Tabs';
+
+
+function App() {
+  const tabs = [
+    {title: "Tab 1", content: "Tab 1 content is showing here"}, 
+    {title: "Tab 2", content: "Tab 2 content is showing here"}, 
+    {title: "Tab 3", content: "Tab 3 content is showing here"}
+  ];
+
+  return (
+    <Tabs tabs={tabs}/>
+  );
+}
+
+export default App;
